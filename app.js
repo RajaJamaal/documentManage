e204668708427bbc53c00e08c8faeb06093bcdc0
RajaJamaal <jamal.bapo@gmail.com>
2023-12-12 03:07:02 -0500

    changes

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ const {
   averageCommentPerDocument,
   latestLogin,
   userWithMostComments,
+  firstCommentUser,
+  totalRowsInAllTables,
+  concatNameAndMail,
+  currentTimestamp,
+  daysSinceLastLogin,
+  joiningYear,
+  averageDocumentCreationTime,
+  userWithMostDocumentUpdates,
 } = require("./queries");
 
 const createDatabase = async (postgres) => {
@@ -33,8 +41,6 @@ const createDatabase = async (postgres) => {
       ["document_db"]
     );
 
-    // await client.query(`DROP DATABASE document_db;`);
-
     if (!exists.rows.length) {
       // Database doesn't exist, so create it
       await client.query(`CREATE DATABASE document_db;`);
@@ -45,8 +51,6 @@ const createDatabase = async (postgres) => {
   } catch (error) {
     console.error(`Error creating or checking database: ${error}`);
   }
-
-  // await client.query(`DROP DATABASE document_db;`);
 
   await client.end(); // close connection to default database
 };
@@ -98,99 +102,133 @@ const connectToDatabase = async (document_db) => {
     console.error("ERROR creating tables", error);
   }
 
-  // for (let i = 0; i < 5000; i++) {
-  //   const element = array[i];
+  console.log("Checking if DB is populated...");
 
-  // }
-  //   let emailCount = 0;
+  const populationCheck = await newClient.query(
+    "SELECT id FROM users WHERE id = 5000;"
+  );
+  if (!populationCheck) {
+    // seeding fake data
+    console.log("DB is empty, populating fake data");
 
-  //   const generateUser = () => ({
-  //     name: faker.internet.userName(),
-  //     email: `${faker.internet.userName()}_${emailCount++}@fakemail.com`,
-  //     join_date: faker.date.past(),
-  //     last_login: faker.date.recent(),
-  //   });
+    for (let i = 0; i < 5000; i++) {
+      const element = array[i];
+    }
+    let emailCount = 0;
 
-  //   const users = Array.from({ length: 5000 }, generateUser);
+    const generateUser = () => ({
+      name: faker.internet.userName(),
+      email: `${faker.internet.userName()}_${emailCount++}@fakemail.com`,
+      join_date: faker.date.past(),
+      last_login: faker.date.recent(),
+    });
 
-  //   const insertUser = async (user) => {
-  //     const newUser = await newClient.query(`INSERT INTO users (name, email, join_date, last_login) VALUES ($1, $2, $3, $4) RETURNING id`, [user.name, user.email, user.join_date, user.last_login]);
-  //     user.id = newUser.rows[0].id;
-  //     console.log(`user ${user.name} added to DB with id: ${user.id}`);
+    const users = Array.from({ length: 5000 }, generateUser);
 
-  //     return user;
-  //   };
+    const insertUser = async (user) => {
+      const newUser = await newClient.query(
+        `INSERT INTO users (name, email, join_date, last_login) VALUES ($1, $2, $3, $4) RETURNING id`,
+        [user.name, user.email, user.join_date, user.last_login]
+      );
+      user.id = newUser.rows[0].id;
+      console.log(`user ${user.name} added to DB with id: ${user.id}`);
 
-  //   for (const user of users) {
-  //     await insertUser(user);
-  //     console.log(` user.name=${user.name}   with user.id=${user.id}  added to DB`);
+      return user;
+    };
 
-  //     const generateDocs = () => ({
-  //       created_by: user.id,
-  //       title: faker.lorem.sentence(5),
-  //       description: faker.lorem.paragraph(5),
-  //       created_at: faker.date.past(),
-  //       updated_at: faker.date.recent(),
-  //     });
+    for (const user of users) {
+      await insertUser(user);
+      console.log(
+        ` user.name=${user.name}   with user.id=${user.id}  added to DB`
+      );
 
-  //     console.log("created_by", user.id);
+      const generateDocs = () => ({
+        created_by: user.id,
+        title: faker.lorem.sentence(5),
+        description: faker.lorem.paragraph(5),
+        created_at: faker.date.past(),
+        updated_at: faker.date.recent(),
+      });
 
-  //     const documents = Array.from({ length: 250 }, generateDocs);
+      console.log("created_by", user.id);
 
-  //     const insertDoc = async (doc) => {
-  //       const newDoc = await newClient.query(`INSERT INTO documents (created_by, title, description, created_at, updated_at) VALUES ($1, $2, $3, $4, $5) RETURNING id`, [doc.created_by, doc.title, doc.description, doc.created_at, doc.updated_at]);
-  //       doc.id = newDoc.rows[0].id;
-  //       console.log(`doc.id=${doc.id} added to DB by user.id=${user.id}`);
+      const documents = Array.from({ length: 250 }, generateDocs);
 
-  //     };
+      const insertDoc = async (doc) => {
+        const newDoc = await newClient.query(
+          `INSERT INTO documents (created_by, title, description, created_at, updated_at) VALUES ($1, $2, $3, $4, $5) RETURNING id`,
+          [
+            doc.created_by,
+            doc.title,
+            doc.description,
+            doc.created_at,
+            doc.updated_at,
+          ]
+        );
+        doc.id = newDoc.rows[0].id;
+        console.log(`doc.id=${doc.id} added to DB by user.id=${user.id}`);
+      };
 
-  //     for (doc of documents){
-  //       await insertDoc(doc);
-  //       console.log(` user.id =${user.id} added doc.id= ${doc.id} to DB`);
-  //     };
+      for (doc of documents) {
+        await insertDoc(doc);
+        console.log(` user.id =${user.id} added doc.id= ${doc.id} to DB`);
+      }
+    }
 
-  //   };
+    const generateComments = () => ({
+      created_by: Math.floor(Math.random() * 5000) + 1,
+      commented_on: Math.floor(Math.random() * 1250000) + 1,
+      title: faker.lorem.sentence(5),
+      description: faker.lorem.paragraph(5),
+      created_at: faker.date.past(),
+      updated_at: faker.date.recent(),
+    });
 
-  //   const generateComments = () => ({
-  //     created_by: Math.floor(Math.random() * 5000) + 1,
-  //     commented_on: Math.floor(Math.random() * 1250000) + 1,
-  //     title: faker.lorem.sentence(5),
-  //     description: faker.lorem.paragraph(5),
-  //     created_at: faker.date.past(),
-  //     updated_at: faker.date.recent(),
-  //   });
+    const comments = Array.from({ length: 30000 }, generateComments);
 
-  //   const comments = Array.from({ length: 30000 }, generateComments);
+    const insertComment = async (comment) => {
+      const newComment = await newClient.query(
+        `INSERT INTO comments (created_by, commented_on, title, description, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6)`,
+        [
+          comment.created_by,
+          comment.commented_on,
+          comment.title,
+          comment.description,
+          comment.created_at,
+          comment.updated_at,
+        ]
+      );
+    };
 
-  //   const insertComment = async (comment) => {
-  //     const newComment = await newClient.query(`INSERT INTO comments (created_by, commented_on, title, description, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6)`, [comment.created_by, comment.commented_on, comment.title, comment.description, comment.created_at, comment.updated_at]);
-  //   };
+    for (comment of comments) {
+      await insertComment(comment);
+      console.log(
+        ` user ${comment.created_by} commented on ${comment.commented_on} to DB`
+      );
+    }
+  }
 
-  //   for (comment of comments){
-  //     await insertComment(comment);
-  //     console.log(` user ${comment.created_by} commented on ${comment.commented_on} to DB`);
-  //   };
-  // };
+  console.log("DB is already populated, skipping data seeding...");
 
   //==============================================================================================================
-  ////// Queries using different types of joins to fetch combined data from the users, documents and comments tables
+  // ////// Queries using different types of joins to fetch combined data from the users, documents and comments tables
 
-  // INNER-JOIN => This query retrieves all documents and their corresponding user and comments
-  // const innerJoinData = await newClient.query(innerJoin);
-  // console.log("inner-Join-Data", innerJoinData.rows);
+  // // INNER-JOIN => This query retrieves all documents and their corresponding user and comments
+  const innerJoinData = await newClient.query(innerJoin);
+  console.log("inner-Join-Data", innerJoinData.rows);
 
-  // LEFT-JOIN => returns a table with all users, even if they haven't created any documents or written any comments. It will also include all documents, even if they haven't received any comments.
-  // const leftJoinData = await newClient.query(leftJoin);
-  // console.log("LEFT-Join-Data", leftJoinData.rows);
+  // // LEFT-JOIN => returns a table with all users, even if they haven't created any documents or written any comments. It will also include all documents, even if they haven't received any comments.
+  const leftJoinData = await newClient.query(leftJoin);
+  console.log("LEFT-Join-Data", leftJoinData.rows);
 
-  // RIGHT-JOIN => return a table with all documents, even if they haven't been created by any users or received any comments.
-  // It will also include all comments including ORPHANS
-  // const rightJoinData = await newClient.query(rightJoin);
-  // console.log("RIGHT-Join-Data", rightJoinData.rows);
+  // // RIGHT-JOIN => return a table with all documents, even if they haven't been created by any users or received any comments.
+  // // It will also include all comments including ORPHANS
+  const rightJoinData = await newClient.query(rightJoin);
+  console.log("RIGHT-Join-Data", rightJoinData.rows);
 
-  // FULL-OUTER-JOIN => returnS a table with all users+documents+comments, regardless of whether they have any relationships with each other
-  // const fullOuterJoinData = await newClient.query(fullOuterJoin);
-  // console.log("FULL-OUTER-join-Data", fullOuterJoinData.rows);
+  // // FULL-OUTER-JOIN => returnS a table with all users+documents+comments, regardless of whether they have any relationships with each other
+  const fullOuterJoinData = await newClient.query(fullOuterJoin);
+  console.log("FULL-OUTER-join-Data", fullOuterJoinData.rows);
   //=========================================================================================================================
   // Using aggregate functions like COUNT, AVG, MAX, MIN, SUM to analyze data (e.g., average number of comments per document).
   // COUNT => Returns the total count of docs in document table
@@ -205,28 +243,55 @@ const connectToDatabase = async (document_db) => {
     `averageCommentPerDocument = ${averageCommPerDoc.rows[0].avg_comments}`
   );
 
-  // MAX()  => using max function on last_login to fetch latest login time
+  // MAX()  => using "max function" on last_login to fetch latest login time
   const latestLoginTime = await newClient.query(latestLogin);
   console.log(`latestLoginTime = ${latestLoginTime.rows[0].latest_login}`);
 
-  let userWithMaxComments;
-  const usersWithMostComments = newClient
-    .query(userWithMostComments, userWithMaxComments)
-    .then((result) => {
-      if (result) {
-        const usersWithMostComments = result.rows;
-        console.log("Users with most comments:");
-        for (const user of usersWithMostComments) {
-          console.log(`- ID: ${user.id}, Name: ${user.name}`);
-        }
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  // MAX()  => using "max function" and "inner queries/sub-queries" to query user with most comments
+  // STRING FUNCTIONS => Using LOWER() to LOWERCASE userName
+  const usersWithMostComments = await newClient.query(userWithMostComments);
+  console.log("usersWithMostComments", usersWithMostComments.rows);
 
-  // MIN()  => using min function to fetch
+  // MIN()  => using "min function" to fetch document title and username of ten earliest comments
+  // STRING FUNCTIONS => Using UPPER() to capitalize userName
+  const firstCommentUserdata = await newClient.query(firstCommentUser);
+  console.log("firstCommentUserdata", firstCommentUserdata.rows);
+
+  // SUM => using "sum function" we are finding the sum of count of data across all tables
+  const SumOfCountOfRowsAcrossDB = await newClient.query(totalRowsInAllTables);
+  console.log("SumOfCountOfRowsAcrossDB", SumOfCountOfRowsAcrossDB.rows);
+
+  // STRING FUNCTIONS => Using "CONCAT()" to stitch name and email of a certain user
+  const concatNameAndMailData = await newClient.query(concatNameAndMail);
+  console.log("concatNameAndMailData", concatNameAndMailData.rows);
+
+  // SQL DATE FUNCTIONS => CURRENT_TIMESTAMP
+  const currentTimestampData = await newClient.query(currentTimestamp);
+  console.log("currentTimestampData", currentTimestampData.rows);
+
+  // SQL DATE FUNCTIONS => using PSQL "EXTRACT() and AGE()" to calculate the number of days since last login of certain users
+  const daysSinceLastLoginData = await newClient.query(daysSinceLastLogin);
+  console.log("daysSinceLastLoginData", daysSinceLastLoginData.rows);
+
+  // SQL DATE FUNCTIONS => using SQL DATENAME(year, join_date) to extract joining year
+  // const joiningYearData = await newClient.query(joiningYear);
+  // console.log("joiningYearData", joiningYearData.rows);
+
+  // / SQL DATE FUNCTIONS => using SQL CTE to calculate average doc creation time of first few users
+  const averageDocumentCreationTimeData = await newClient.query(
+    averageDocumentCreationTime
+  );
+  console.log(
+    "averageDocumentCreationTimeData",
+    averageDocumentCreationTimeData.rows
+  );
+
+  // / SQL DATE FUNCTIONS => using SQL CTE to calculate users with most doc updates
+  const userWithMostDocumentUpdatesData = await newClient.query(userWithMostDocumentUpdates);
+  console.log("userWithMostDocumentUpdatesData =",userWithMostDocumentUpdatesData.rows);
+
 };
+
 createDatabase("document_db")
   .then(() => connectToDatabase("document_db"))
   .catch(console.error);
--- queries.js
+++ queries.js
@@ -29,22 +29,22 @@ const createCommentsTable = `CREATE TABLE comments (
 const innerJoin = `SELECT * FROM users 
     INNER JOIN documents ON users.id = documents.created_by 
     INNER JOIN comments ON documents.id = comments.commented_on
-    LIMIT 10;`;
+    LIMIT 5;`;
 
 const leftJoin = `SELECT * FROM users 
     LEFT JOIN documents ON users.id = documents.created_by
     LEFT JOIN comments ON documents.id = comments.commented_on
-    LIMIT 10;`;
+    LIMIT 3;`;
 
 const rightJoin = `SELECT * FROM users 
     RIGHT JOIN documents ON users.id = documents.created_by
     RIGHT JOIN comments ON documents.id = comments.commented_on
-    LIMIT 10;`;
+    LIMIT 3;`;
 
 const fullOuterJoin = `SELECT * FROM users
     FULL OUTER JOIN documents ON users.id = documents.created_by
     FULL OUTER JOIN comments ON documents.id = comments.commented_on
-    LIMIT 10;`;
+    LIMIT 3;`;
 
 const countDocuments = `SELECT COUNT(*) AS "Total number of records" FROM documents;`;
 
@@ -56,28 +56,97 @@ const averageCommentPerDocument = `SELECT AVG(comment_count) as avg_comments
     GROUP BY documents.id
     ) as comment_counts;`;
 
-const latestLogin =`SELECT MAX(last_login) AS latest_login
+const latestLogin = `SELECT MAX(last_login) AS latest_login
     FROM users;
     `;
 
-const userWithMostComments = `SELECT u.id, u.name
+const userWithMostComments = `SELECT u.id, LOWER(u.name), c.comments_count
     FROM users AS u
     INNER JOIN (
-    SELECT created_by, COUNT(*) AS max_comments
+    SELECT created_by, COUNT(*) AS comments_count
     FROM comments
      GROUP BY created_by
         ) AS c ON u.id = c.created_by
-        WHERE c.max_comments = (
-        SELECT MAX(max_comments)
+        WHERE c.comments_count = (
+        SELECT MAX(comments_count)
             FROM (
-            SELECT created_by, COUNT(*) AS max_comments
+            SELECT created_by, COUNT(*) AS comments_count
             FROM comments
             GROUP BY created_by
             ) AS inner_subquery
     );
     `;
 
+const firstCommentUser = `SELECT UPPER(users.name), MIN(comments.created_at) as first_comment_time
+    FROM comments
+    INNER JOIN users ON comments.created_by = users.id
+    GROUP BY users.name
+    ORDER BY first_comment_time
+    LIMIT 1;`;
 
+const totalRowsInAllTables = `SELECT 
+    (SELECT COUNT(*) FROM users) +
+    (SELECT COUNT(*) FROM documents) +
+    (SELECT COUNT(*) FROM comments) AS total_rows;`;
+
+const concatNameAndMail = `SELECT CONCAT(name, ',  ' , email) AS concat_Name_Email FROM users WHERE name = 'Regan_Kertzmann' LIMIT 2;`;
+
+const currentTimestamp = `SELECT CURRENT_TIMESTAMP;`;
+
+const daysSinceLastLogin = `SELECT name, EXTRACT(DAY FROM AGE(CURRENT_TIMESTAMP, last_login)) AS days_since_last_login
+    FROM users
+    ORDER BY 
+    last_login DESC
+    LIMIT 3;
+    `;
+
+const joiningYear = `SELECT DATENAME(year, join_date) AS joining_year
+    FROM users WHERE email="Anderson_Nienow74_149@fakemail.com";`;
+
+const averageDocumentCreationTime = `WITH DocumentCreationTimes AS (
+        SELECT 
+            created_by,
+            AVG(EXTRACT(EPOCH FROM (created_at - join_date))) AS avg_creation_time_seconds
+        FROM 
+            documents
+        INNER JOIN 
+            users ON documents.created_by = users.id
+        GROUP BY 
+            created_by
+    )
+    SELECT 
+        u.id AS user_id,
+        u.name AS user_name,
+        DCT.avg_creation_time_seconds
+    FROM 
+        users u
+    LEFT JOIN 
+        DocumentCreationTimes DCT ON u.id = DCT.created_by
+    LIMIT 3;
+    `;
+
+const userWithMostDocumentUpdates = `WITH UserUpdatedDocumentCount AS (
+    SELECT 
+        created_by,
+        COUNT(updated_at) AS updated_document_count
+    FROM 
+        documents
+    GROUP BY 
+        created_by
+    ORDER BY 
+        updated_document_count DESC
+    LIMIT 10
+    )
+    SELECT 
+    u.id AS user_id,
+    u.name AS user_name,
+    COALESCE(UDC.updated_document_count, 0) AS updated_document_count
+    FROM 
+    users u
+    LEFT JOIN 
+    UserUpdatedDocumentCount UDC ON u.id = UDC.created_by
+    LIMIT 3;
+    `;
 module.exports = {
   createUsersTable,
   createDocumentsTable,
@@ -90,4 +159,12 @@ module.exports = {
   averageCommentPerDocument,
   latestLogin,
   userWithMostComments,
+  firstCommentUser,
+  totalRowsInAllTables,
+  concatNameAndMail,
+  currentTimestamp,
+  daysSinceLastLogin,
+  joiningYear,
+  averageDocumentCreationTime,
+  userWithMostDocumentUpdates,
 };
